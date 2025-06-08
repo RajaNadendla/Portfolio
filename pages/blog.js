f7507a2235f377e9d5a723db2c47ce0dbb15a511
RajaNadendla <raja@rajanadendla.com>
2025-06-08 17:23:34 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,8 +1,12 @@
+import Layout from '@/components/Layout';
+
 export default function Blog() {
   return (
-    <div className="text-white">
-      <h1 className="text-3xl font-bold mb-4">Blog</h1>
-      <p className="text-gray-400">This is the blog page. More content coming soon.</p>
-    </div>
+    <Layout>
+      <div className="max-w-3xl mx-auto p-6">
+        <h1 className="text-4xl font-bold mb-4">Blog</h1>
+        <p className="text-gray-400">This is the blog page. More content coming soon.</p>
+      </div>
+    </Layout>
   );
 }
