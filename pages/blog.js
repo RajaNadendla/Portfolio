152b79605806d631c210c882a0cfb0f192d16156
RajaNadendla <raja@rajanadendla.com>
2025-06-08 22:10:13 +0530

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -1,12 +1,5 @@
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
 
 export default function Blog() {
-  return (
-    <Layout>
-      <div className="container">
-        <h1>Blog</h1>
-        <p>Coming soon... Stay tuned for insights on Product Management, Search, and AI.</p>
-      </div>
-    </Layout>
-  )
+  return <Layout><h1>Blog</h1><p>Coming soon. Posts on AI, product thinking, and search.</p></Layout>;
 }
