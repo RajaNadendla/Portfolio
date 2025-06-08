8ae37a1e453a0438c6517b07ca3f6009437507e9
RajaNadendla <raja@rajanadendla.com>
2025-06-08 22:09:48 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,5 @@ import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 
 export default function Home() {
-  return (
-    <Layout>
-      <Hero />
-    </Layout>
-  );
+  return <Layout><Hero /></Layout>;
 }
