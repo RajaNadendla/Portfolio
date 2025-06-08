760edaa5eac90cc512e3cfcc58e1f9aef0be1f29
RajaNadendla <raja@rajanadendla.com>
2025-06-08 17:12:17 +0530

    Add files via upload

---
+++ pages/projects.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+  return (
+    <div className="text-white">
+      <h1 className="text-3xl font-bold mb-4">Projects</h1>
+      <p className="text-gray-400">This is the projects page. More content coming soon.</p>
+    </div>
+  );
+}
